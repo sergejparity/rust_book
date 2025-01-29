815bf7e0425cf1556555eff34c228f88213363c8
Sergejs Kostjucenko <sergejs.kostjucenko+p@gmail.com>
2025-01-29 14:35:07 +0200

    add ownership ch4.1

---
+++ chapter4/ownership_4_1/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let i1 = 10;
+    let i2 = i1 + 2;
+
+    println!("i1 is {} and i2 is {}", i1, i2);
+
+    let s1 = String::from("hello");
+    let s2 = s1 + " world";
+
+    println!("s1 is {} and s2 {}", s2, s2);
+    println!("i1 is {} and i2 is {}", i1, i2);
+}
