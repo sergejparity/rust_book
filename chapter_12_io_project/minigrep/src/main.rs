50684c92b68c51f882940a273319169e269407e3
Sergejs Kostjucenko <sergejs.kostjucenko+p@gmail.com>
2025-02-04 15:49:37 +0200

    improve configuration parising

--- chapter_12_io_project/minigrep/src/main.rs
+++ chapter_12_io_project/minigrep/src/main.rs
@@ -1,17 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    // dbg!(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.clone().query);
+    println!("In file {}", config.clone().file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+#[derive(Clone)]
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
